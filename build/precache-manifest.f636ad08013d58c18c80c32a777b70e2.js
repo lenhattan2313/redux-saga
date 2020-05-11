7a2000ad3c1d6da663629bd7e3a403a6b43a603c
lenhattan2313 <lenhattan2313@gmail.com>
2020-05-11 15:34:14 +0700

    build

---
+++ build/precache-manifest.f636ad08013d58c18c80c32a777b70e2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01041347d4a41543c6a142fabce71aa3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "80fbe97917b254522e3b",
+    "url": "/static/css/2.f1d36a8f.chunk.css"
+  },
+  {
+    "revision": "80fbe97917b254522e3b",
+    "url": "/static/js/2.c0dc5cad.chunk.js"
+  },
+  {
+    "revision": "b95d735f9bdac369c512281af49352ca",
+    "url": "/static/js/2.c0dc5cad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6661b0dd4ab05c9888d5",
+    "url": "/static/js/main.56dd261d.chunk.js"
+  },
+  {
+    "revision": "d15a9518a9f4e76775e2",
+    "url": "/static/js/runtime-main.eb4c5cbf.js"
+  },
+  {
+    "revision": "1b1a27ca8cc684cea5b9c28f179b09f6",
+    "url": "/static/media/loading.1b1a27ca.gif"
+  }
+]);
